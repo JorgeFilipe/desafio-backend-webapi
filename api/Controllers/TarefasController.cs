2c2ace25d3f269f2eacd128d96a9d06bfce0e1c3
Jorge Filipe <jorgefilipedasilva@gmail.com>
2020-10-02 03:55:58 -0300

    Update TarefasController.cs

--- api/Controllers/TarefasController.cs
+++ api/Controllers/TarefasController.cs
@@ -1,5 +1,22 @@
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
 namespace api.Controllers {
-    public class TarefasController {
-        
+
+    [Route ("api/[Controller]")]
+    public class TarefasController : ControllerBase {
+
+        private readonly DataContext _context;
+
+        public TarefasController (DataContext context) {
+            this._context = context;
+        }
+
+        [HttpGet ("listartarefas")]
+        public async Task<ActionResult> listarfilmes () {
+            var tarefas = await _context.Tarefas.ToListAsync ();
+            return Ok (tarefas);
+        }
     }
 }
