67060b160de1a04885ec77ae3e1321d7149d4b67
Jorge Filipe <jorgefilipedasilva@gmail.com>
2020-10-02 03:21:23 -0300

    Update Tarefa.cs
    Criando os atributos da classe Tarefa.cs

--- api/Models/Tarefa.cs
+++ api/Models/Tarefa.cs
@@ -1,7 +1,21 @@
-namespace api.Models
-{
-    public class Tarefa
-    {
-        
+namespace api.Models {
+    public class Tarefa {
+        [Key]
+        public int Id { get; set; }
+
+        [MinLength (1, ErrorMessage = "O título da tarefa deve conter no mínimo 1 caracter")]
+        [MaxLength (80, ErrorMessage = "O título da tarefa deve conter no máximo 80 caracteres")]
+        [Required (ErrorMessage = "Titulo obrigatorio")]
+        public string Titulo { get; set; }
+
+        [MinLength (20, ErrorMessage = "A descrição da tarefa deve conter no mínimo 20 caracteres")]
+        [MaxLength (160, ErrorMessage = "A descrição da tarefa deve conter no máximo 160 caracteres")]
+        public string Descricao { get; set; }
+
+        [Required (ErrorMessage = "Data de Crição obrigatória")]
+        public string DataCriacao { get; set; }
+
+        [Required (ErrorMessage = "Data de Atualização obrigatória")]
+        public string DataAtualizacao { get; set; }
     }
 }
