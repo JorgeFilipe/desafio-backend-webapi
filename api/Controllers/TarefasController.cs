6bb3d97b74bc7b6a33cfba41b44e55e4f2489669
Jorge Filipe <jorgefilipedasilva@gmail.com>
2020-10-02 12:09:27 -0300

    Adicionando o método buscar no TarefasController.cs

--- api/Controllers/TarefasController.cs
+++ api/Controllers/TarefasController.cs
@@ -22,10 +22,27 @@ namespace api.Controllers {
             this._context = context;
         }
 
+        // https://localhost:5001/tarefas/listarTarefas
+        // Lista todas as tarefas
         [HttpGet ("listarTarefas")]
         public async Task<ActionResult> GetTarefas() {
             var tarefas = await _context.Tarefas.ToListAsync ();
             return Ok (tarefas);
+        }
+
+        // https://localhost:5001/tarefas/buscarTarefa/{id}
+        // Busca uma tarefa por Id
+        [HttpGet("buscarTarefa/{id}")]
+        public async Task<ActionResult<TarefasController>> id(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tarefa);
         }
     }
 }
